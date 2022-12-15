da317ce194d2e91fd869a3b9b7e6a458652988b9
valerieyang00 <valerieyang00@gmail.com>
2022-12-15 14:03:37 -0800

    complete exercise

--- section6-exercise3.js
+++ section6-exercise3.js
@@ -11,3 +11,32 @@
 // sameFrequency(34,14) // false
 // sameFrequency(3589578, 5879385) // true
 // sameFrequency(22,222) // false
+
+function sameFrequency(numOne, numTwo){
+  let frequency = {}
+  const strOne = numOne.toString()
+  const strTwo = numTwo.toString()
+
+  if (strOne.length !== strTwo.length) {
+    return false
+  }
+
+  for (let char in strOne) {
+      let digit = strOne[char]
+      frequency[digit] ? frequency[digit] ++ : frequency[digit] = 1
+  }
+
+
+  for (let char in strTwo) {
+    let digit = strTwo[char]
+    if (!frequency[digit]) {
+        return false
+    } else {
+        frequency[digit]--
+    }
+}
+  
+  return true
+}
+
+sameFrequency(322,323)
